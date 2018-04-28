5f992f0e74927ae44c4a1993f5e8fa9b16e75267
Jared Tulayan <2lyNJ.E@gmail.com>
2018-04-28 18:27:05 -0400

    Add another round function to round to the nearest multiple

--- src/com/jtulayan/util/Mathf.java
+++ src/com/jtulayan/util/Mathf.java
@@ -20,4 +20,15 @@ public class Mathf {
 
         return Math.round(val * tens) / tens;
     }
+
+    /**
+     * Rounds the specified value to the nearest specified multiple
+     *
+     * @param val    the number to round
+     * @param multiple the multiple to round val to
+     * @return number rounded to nearest multiple
+     */
+    public static double round(double val, double multiple) {
+        return Math.round(val / multiple) * multiple;
+    }
 }
